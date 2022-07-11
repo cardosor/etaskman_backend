6679070c2cea5f9a6239ab84552c7298578838ad
Rafael Cardoso <usacb@hotmail.com>
2022-07-11 00:52:50 -0500

    fix usersController path

--- routes/api/Users.js
+++ routes/api/Users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const usersController = require('../../controllers/api/users');
+const usersController = require('../../controllers/api/Users');
 const checkUserLogged = require('../../config/checkUserLogged');
 
 //INDUCES
